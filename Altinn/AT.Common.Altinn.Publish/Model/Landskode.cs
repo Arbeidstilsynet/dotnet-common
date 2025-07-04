0cb0021715085e5b0084c07e70cb7356caa28f59
Bjørnar Prytz <bjornar.groholt.prytz@arbeidstilsynet.no>
2025-07-04 15:21:28 +0200

    fix json deserialization

--- Altinn/AT.Common.Altinn.Publish/Model/Landskode.cs
+++ Altinn/AT.Common.Altinn.Publish/Model/Landskode.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace Arbeidstilsynet.Common.Altinn.Model;
 
 /// <summary>
@@ -5,4 +7,7 @@ namespace Arbeidstilsynet.Common.Altinn.Model;
 /// </summary>
 /// <param name="Land">The country name in English, e.g. "Norway".</param>
 /// <param name="Kode">International dialing code, e.g. "+47" for Norway.</param>
-public record Landskode(string Land, string Kode);
+public record Landskode(
+    [property: JsonPropertyName("land")] string Land,
+    [property: JsonPropertyName("kode")] string Kode
+);
