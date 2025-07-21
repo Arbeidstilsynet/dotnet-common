e8f9a324b5946794d9989f2900867d8569c9e1f7
Maurice Kilgus <maurice.kilgus@arbeidstilsynet.no>
2025-07-21 12:48:38 +0200

    fix: archunit tests

--- Altinn/AT.Common.Altinn.Publish/Model/Adapter/AltinnAppConfiguration.cs
+++ Altinn/AT.Common.Altinn.Publish/Model/Adapter/AltinnAppConfiguration.cs
@@ -1,3 +1,5 @@
+namespace Arbeidstilsynet.Common.Altinn.Model.Adapter;
+
 public record AltinnAppConfiguration(string? MainDocumentDataTypeName = null)
 {
     public string MainDocumentDataTypeName { get; init; } = MainDocumentDataTypeName ?? "skjema";
