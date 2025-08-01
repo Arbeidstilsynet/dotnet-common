2c38da1144f08e1f5afda7bdf9572fd7f857729f
Maurice Kilgus <maurice.kilgus@arbeidstilsynet.no>
2025-08-01 10:22:56 +0200

    fix: use calling assembly instead of executing at internal controller check

--- AspNetCore/AT.Common.AspNetCore.Publish/CrossCutting/CustomControllerFeatureProvider.cs
+++ AspNetCore/AT.Common.AspNetCore.Publish/CrossCutting/CustomControllerFeatureProvider.cs
@@ -9,12 +9,12 @@ internal class CustomControllerFeatureProvider : ControllerFeatureProvider
     protected override bool IsController(TypeInfo typeInfo)
     {
         var isControllerWithinExecutingAssembly =
-            Assembly.GetExecutingAssembly() == typeInfo.Assembly;
+            Assembly.GetCallingAssembly() == typeInfo.Assembly;
         var isInternalController =
             !typeInfo.IsAbstract
             && !typeInfo.IsPublic
             && typeInfo.IsDefined(typeof(ControllerAttribute));
-        // if a controller is internal it should only be added when it is a part of the executing assembly
+        // if a controller is internal it should only be added when it is a part of the calling assembly
         return (isControllerWithinExecutingAssembly && isInternalController)
             || base.IsController(typeInfo);
     }
