1986241524e8a895dec2ea09c2e52020830cd097
Maurice Kilgus <maurice.kilgus@arbeidstilsynet.no>
2025-08-15 14:34:37 +0200

    feat: basic token cache

--- Altinn/AT.Common.Altinn.Publish/Implementation/Token/AltinnTokenProvider.cs
+++ Altinn/AT.Common.Altinn.Publish/Implementation/Token/AltinnTokenProvider.cs
@@ -2,6 +2,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using Arbeidstilsynet.Common.Altinn.DependencyInjection;
+using Arbeidstilsynet.Common.Altinn.Model.Api;
 using Arbeidstilsynet.Common.Altinn.Ports.Clients;
 using Arbeidstilsynet.Common.Altinn.Ports.Token;
 using Microsoft.Extensions.Options;
@@ -14,14 +15,31 @@ internal class AltinnTokenProvider(
     IAltinnAuthenticationClient altinnAuthenticationClient
 ) : IAltinnTokenProvider
 {
+    private MaskinportenTokenResponse? _currentToken;
+
+    private DateTime _lastUpdate = DateTime.Now;
+
     public async Task<string> GetToken()
     {
-        // get maskinporten token
-        var maskinportenToken = await maskinportenClient.GetToken();
+        if (_currentToken != null)
+        {
+            var timeDiff = DateTime.Now - _lastUpdate;
+            if (timeDiff.Seconds >= _currentToken.ExpiresIn)
+            { // get maskinporten token
+                _currentToken = await maskinportenClient.GetToken();
+                _lastUpdate = DateTime.Now;
+            }
+        }
+        else
+        {
+            // get maskinporten token
+            _currentToken = await maskinportenClient.GetToken();
+            _lastUpdate = DateTime.Now;
+        }
         // get altinn token
         return await altinnAuthenticationClient.ExchangeToken(
             Model.Api.AuthenticationTokenProvider.Maskinporten,
-            maskinportenToken.AccessToken
+            _currentToken.AccessToken
         );
     }
 }
