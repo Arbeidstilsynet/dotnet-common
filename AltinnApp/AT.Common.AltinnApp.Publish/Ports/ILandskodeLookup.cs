24b5688a94aa1429a1be2a65a8b6d4c51276c146
Bjørnar Prytz <bjornar.groholt.prytz@arbeidstilsynet.no>
2025-10-31 15:24:08 +0100

    rename parameter for clarity

--- AltinnApp/AT.Common.AltinnApp.Publish/Ports/ILandskodeLookup.cs
+++ AltinnApp/AT.Common.AltinnApp.Publish/Ports/ILandskodeLookup.cs
@@ -10,9 +10,9 @@ public interface ILandskodeLookup
     /// <summary>
     /// Get a country code and name based on a 3-letter ISO code.
     /// </summary>
-    /// <param name="isoCode">3-letter ISO code for the country</param>
+    /// <param name="alpha3Code">3-letter ISO code for the country</param>
     /// <returns>The Landskode matching the given ISO code, or null if not found.</returns>
-    Task<Landskode?> GetLandskode(string isoCode);
+    Task<Landskode?> GetLandskode(string alpha3Code);
 
     /// <summary>
     /// Get all country codes and names.
