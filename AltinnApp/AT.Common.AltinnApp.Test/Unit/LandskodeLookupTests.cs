3924ac4ea8e972661914de26bc4c15b6f9ef4d99
Bjørnar Prytz <bjornar.groholt.prytz@arbeidstilsynet.no>
2025-10-31 14:30:09 +0100

    fix tests

--- AltinnApp/AT.Common.AltinnApp.Test/Unit/LandskodeLookupTests.cs
+++ AltinnApp/AT.Common.AltinnApp.Test/Unit/LandskodeLookupTests.cs
@@ -44,7 +44,7 @@ public class LandskodeLookupTests
             new object[] { "SVN", new Landskode("Slovenia", "+386", "SI", "SVN") },
             new object[] { "ESP", new Landskode("Spain", "+34", "ES", "ESP") },
             new object[] { "SWE", new Landskode("Sweden", "+46", "SE", "SWE") },
-            new object[] { "CZE", new Landskode("Czech Republic", "+420", "CZ", "CZE") },
+            new object[] { "CZE", new Landskode("Czechia", "+420", "CZ", "CZE") },
             new object[] { "DEU", new Landskode("Germany", "+49", "DE", "DEU") },
             new object[] { "HUN", new Landskode("Hungary", "+36", "HU", "HUN") },
             new object[] { "AUT", new Landskode("Austria", "+43", "AT", "AUT") },
@@ -55,7 +55,7 @@ public class LandskodeLookupTests
     [Fact]
     public async Task GetLandskoder_Returns_Alle_Landskoder()
     {
-        const int antallLandskoder = 238;
+        const int antallLandskoder = 253;
 
         var landskoder = (await _sut.GetLandskoder()).ToList();
 
