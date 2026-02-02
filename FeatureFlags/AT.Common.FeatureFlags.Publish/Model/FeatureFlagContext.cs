24076de03e70955b417080f4019b12725c94b723
Roberth Tollefsen <roberth.tollefsen@arbeidstilsynet.no>
2026-02-02 11:11:39 +0100

    legg til ekstra felter i context

--- FeatureFlags/AT.Common.FeatureFlags.Publish/Model/FeatureFlagContext.cs
+++ FeatureFlags/AT.Common.FeatureFlags.Publish/Model/FeatureFlagContext.cs
@@ -4,7 +4,6 @@ namespace Arbeidstilsynet.Common.FeatureFlags.Model;
 
 /// <summary>
 /// Represents the context for feature flag evaluation.
-/// Only supports UserId at the moment.
 /// </summary>
 public class FeatureFlagContext
 {
@@ -12,4 +11,14 @@ public class FeatureFlagContext
     /// Gets or sets the user identifier for feature flag evaluation.
     /// </summary>
     public string? UserId { get; set; }
+
+    /// <summary>
+    /// Gets og sets the AppName for feature flag evaluation.
+    /// </summary>
+    public string? AppName { get; set; }
+
+    /// <summary>
+    /// Gets or sets the SessionId for feature flag evaluation.
+    /// </summary>
+    public string? SessionId { get; set; }
 }
